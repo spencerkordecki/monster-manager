003e89dc4dd970b281c7c354395e9b6dd4f165c1
Spencer Kordecki <spencerkordecki@gmail.com>
2019-01-23 19:32:32 -0500

    build site for github pages

--- src/containers/ManagerApp.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Manager from '../pages/Manager';
-
-const ManagerApp = () => <Manager />;
-
-export default ManagerApp;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import ManagerApp from './containers/ManagerApp';
-import './styles/index.css';
-
-render(<ManagerApp />, document.getElementById('content'));
